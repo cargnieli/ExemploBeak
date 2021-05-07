182eaf18126f3b4b1f1aa62b847d0d9ba55df27e
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-05-07 20:35:41 -0300

    Initial commit

---
+++ src/com/local/ExemploBreak.java
@@ -0,0 +1,20 @@
+package com.local;
+import java.util.Scanner;
+
+public class ExemploBreak {
+
+    public static void main(String[] args) {
+	Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite um número entre: ");
+        int divisor = entrada.nextInt();
+
+        for (int i = 100; i <= 200; i++) {
+            if (i % divisor == 0) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("Fim do Programa");
+    }
+}
